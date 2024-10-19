442506e2606551ea7c88542af6ac00611487c062
ashiqur_rahman_biplop <biplops22@gmail.com>
2024-10-20 04:33:34 +0600

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const port = process.env.PORT || 5000;
 
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 // middleware
-app.use(cors());
+app.use(cors()); 
 app.use(express.json());
 const username = process.env.USER_NAME;
 const password = process.env.PASSWORD;
